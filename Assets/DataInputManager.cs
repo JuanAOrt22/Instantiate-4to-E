b7c7866630155c15b9226f661f638c6dcb5425b5
ATAEFE JUAN ARIEL <46956717@ort>
2022-05-10 14:12:55 -0300

    HacerloTodoBien

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text TxtUsername;
+    
+    public void ShowGreetings(){
+    Debug.Log("Hola " + TxtUsername.text);
+    }
+
+    public void ShowDouble(){
+    
+    if(TxtUsername.text != "")
+        {
+        Debug.Log(int.Parse(TxtUsername.text) * 2);
+        }
+    else
+        {
+        Debug.LogError("Invalid Input: Empty");
+        }
+    }
+}
